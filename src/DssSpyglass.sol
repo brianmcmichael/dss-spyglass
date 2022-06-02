ab284384023aeb5551975eb01e6b3b1de934c0d7
Brian McMichael <brian@brianmcmichael.com>
2022-06-02 13:20:47 -0400

    simple impl

--- src/DssSpyglass.sol
+++ src/DssSpyglass.sol
@@ -1,5 +1,18 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.6;
 
+interface OsmLike {
+    function peek() external view returns (bytes32, bool);
+    function peep() external view returns (bytes32, bool);
+}
+
 contract DssSpyglass {
+
+    function peek(address osm) external view returns (bytes32, bool) {
+        return OsmLike(osm).peek();
+    }
+
+    function peep(address osm) external view returns (bytes32, bool) {
+        return OsmLike(osm).peep();
+    }
 }
